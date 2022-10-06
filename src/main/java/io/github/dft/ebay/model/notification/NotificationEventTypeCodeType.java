bf5282dcb6ce44164cdb9e7d1b64e0a9a2c07bf3
krutik <krutik.khunt@eavira.com>
2022-10-06 20:51:23 +0530

    Ebay Order, Order Ebay API

---
+++ src/main/java/io/github/dft/ebay/model/notification/NotificationEventTypeCodeType.java
@@ -0,0 +1,12 @@
+package io.github.dft.ebay.model.notification;
+
+public class NotificationEventTypeCodeType {
+    public static final String AUCTION_CHECKOUT_COMPLETE = "AuctionCheckoutComplete";
+    public static final String ITEM_SOLD = "ItemSold";
+    public static final String ITEM_UNSOLD = "ItemUnsold";
+    public static final String ITEM_SUSPENDED = "ItemSuspended";
+    public static final String ITEM_CLOSED = "ItemClosed";
+    public static final String ITEM_LISTED = "ItemListed";
+    public static final String ITEM_REVISED = "ItemRevised";
+    public static final String ITEM_EXTENDED = "ItemExtended";
+}
