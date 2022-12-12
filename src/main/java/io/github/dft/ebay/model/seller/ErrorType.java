2e89ad49e589571e3f173905d9c56d1267bc33d2
Dhruv.Fefar <dhruv.fefar@dakshyafreondtech.com>
2022-12-12 14:41:06 +0530

    Additem : Error Message Response add in ReviseFixedPriceItem and AddFixedPrice

--- src/main/java/io/github/dft/ebay/model/seller/ErrorType.java
+++ src/main/java/io/github/dft/ebay/model/seller/ErrorType.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.Data;
-
 import java.util.List;
 
 @Data
