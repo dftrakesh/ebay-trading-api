940c8d008dda56756f5600d7f1eef0e7bd587e5f
Rakesh Dobariya <rakesh.dobariya@dakshyafreondtech.com>
2023-06-09 15:09:19 +0530

    RAG-190: fix price update issue

--- src/main/java/io/github/dft/ebay/model/order/transactions/StartPrice.java
+++ src/main/java/io/github/dft/ebay/model/order/transactions/StartPrice.java
@@ -4,14 +4,13 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlText;
 import lombok.Data;
-
 import java.io.Serializable;
 
 @Data
 @JacksonXmlRootElement(localName = "StartPrice")
 public class StartPrice implements Serializable {
 
-    @JacksonXmlProperty(localName = "currencyID")
+    @JacksonXmlProperty(localName = "currencyID", isAttribute = true)
     private String currencyID;
 
     @JacksonXmlText
