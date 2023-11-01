c1bc6d16bc14839f002518f589c720d9a5023815
krutik <krutik.khunt@eavira.com>
2023-11-01 12:41:55 +0530

    add exchange rate

--- src/main/java/io/github/dft/ebay/model/finance/Amount.java
+++ src/main/java/io/github/dft/ebay/model/finance/Amount.java
@@ -1,13 +1,21 @@
 package io.github.dft.ebay.model.finance;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Amount {
 
     @JsonProperty("value")
     private String value;
     @JsonProperty("currency")
     private String currency;
+    @JsonProperty("convertedFromValue")
+    private String convertedFromValue;
+    @JsonProperty("convertedFromCurrency")
+    private String convertedFromCurrency;
+    @JsonProperty("exchangeRate")
+    private String exchangeRate;
 }
--- src/main/java/io/github/dft/ebay/model/finance/MarketplaceFee.java
+++ src/main/java/io/github/dft/ebay/model/finance/MarketplaceFee.java
@@ -1,9 +1,11 @@
 package io.github.dft.ebay.model.finance;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MarketplaceFee {
 
     @JsonProperty("feeType")
